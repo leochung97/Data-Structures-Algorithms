e3b109731a22af2cd00b7571f7195e10bfcf812c
Leo Chung <leo.chung97@gmail.com>
2022-08-01 15:18:08 -0400

    Added uncompress to Algorithms

--- Algorithms.py
+++ Algorithms.py
@@ -18,3 +18,25 @@ def is_prime(n):
 # print(is_prime(-5)) # -> False
 # print(is_prime(713)) # -> False
 
+# uncompress is O(n * m) time complexity and space complexity; n represents the number of "number groups" while m represents the maximum number within a number group; Space complexity is also O(n * m) due to the output variable that is saved dependent on n and m
+def uncompress(str):
+  numbers = "0123456789"
+  # We use a list / array here because in Python, strings are immutable and must create a NEW copy when strings are concatenated! Thus, you should use an array here as it is mutable and therefore more efficient
+  result = []
+  i = 0
+  j = 0
+  while j < len(str):
+    if str[j] in numbers:
+      j += 1
+    else:
+      # Note: In Python, you can change a string to an integer using int()
+      num = int(str[i:j])
+      # Note: In Python, you can use .append to add to the end of a list / array
+      result.append(str[j] * num)
+      j += 1
+      i = j
+
+  # This line is outside of the while loop - this is linear (O(n))
+  return ''.join(result)
+  
+
