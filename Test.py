a07ce63a93663fb3f6ed98001ed94b33f18a780e
Leo Chung <leo.chung97@gmail.com>
2022-09-19 11:15:18 -0400

    Revised Test.py

--- Test.py
+++ Test.py
@@ -1,12 +1,9 @@
 def addBinary(a, b):
-    x, y = int(a, 2), int(b, 2)
+  x, y = int(a, 2), int(b, 2)
 
-    while y:
-        x, y = x ^ y, (x & y) << 1
-        print(x ^ y)
-        print(x & y)
+  while y:
+    x, y = x ^ y, (x & y) << 1
 
-    return bin(x)[2:]
-
+  return bin(x)[2:]
 
 print(addBinary("11", "1"))
