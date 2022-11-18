8db31fb7c52bfa8e8a7ac7fdd9df5c2086cca189
Leo Chung <leo.chung97@gmail.com>
2022-11-18 17:18:15 -0500

    Add another example for #15 3Sum

--- Leetcode.py
+++ Leetcode.py
@@ -91,6 +91,22 @@ class Solution:
 
     return results
 
+"""
+class Solution:
+  def threeSum(self, nums: List[int]) -> List[List[int]]:
+    res, dups = set(), set()
+    seen = {}
+    for i, val1 in enumerate(nums):
+      if val1 not in dups:
+        dups.add(val1)
+        for j, val2 in enumerate(nums[i+1:]):
+          complement = -val1 - val2
+          if complement in seen and seen[complement] == i:
+            res.add(tuple(sorted((val1, val2, complement))))
+          seen[val2] = i
+      return res
+"""
+
 # 20. Valid Parentheses
 class Solution:
   # Time complexity: O(n) because you run through the entire string
